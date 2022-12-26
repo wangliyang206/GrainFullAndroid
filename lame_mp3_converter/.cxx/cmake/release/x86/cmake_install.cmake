6b43d468669dab686321b4230feb017257ecb10f
DESKTOP-LG1R77I\Administrator <wangliyang206@163.com>
2022-12-26 12:34:48 +0800

    内容变更： 1、修改音频转换注释。

--- lame_mp3_converter/.cxx/cmake/release/x86/cmake_install.cmake
+++ lame_mp3_converter/.cxx/cmake/release/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/git/Mp3Converter-master/library
+# Install script for directory: E:/AndroidStudioGit/GrainFullAndroid/lame_mp3_converter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/git/Mp3Converter-master/library/.cxx/cmake/release/x86/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/AndroidStudioGit/GrainFullAndroid/lame_mp3_converter/.cxx/cmake/release/x86/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
