8cf77f4c849c8a40733e5c2478701b7baf17bd31
wangliyang <wangliyang206@163.com>
2023-01-05 17:49:49 +0800

    内容变更： 1、完善 图片取色器 功能。

--- app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/activity/PicturePipetteActivity.java
+++ app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/activity/PicturePipetteActivity.java
@@ -1,36 +1,43 @@
 package com.zqw.mobile.grainfull.mvp.ui.activity;
 
+import static com.jess.arms.utils.Preconditions.checkNotNull;
+
+import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.jess.arms.di.component.AppComponent;
+import com.blankj.utilcode.util.ClipboardUtils;
+import com.blankj.utilcode.util.ColorUtils;
 import com.jess.arms.base.BaseActivity;
+import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
-
-import static com.jess.arms.utils.Preconditions.checkNotNull;
-
+import com.lcw.library.imagepicker.ImagePicker;
+import com.zqw.mobile.grainfull.R;
+import com.zqw.mobile.grainfull.app.global.Constant;
 import com.zqw.mobile.grainfull.app.utils.ColorsUtil;
+import com.zqw.mobile.grainfull.app.utils.GlideLoader;
 import com.zqw.mobile.grainfull.di.component.DaggerPicturePipetteComponent;
 import com.zqw.mobile.grainfull.mvp.contract.PicturePipetteContract;
 import com.zqw.mobile.grainfull.mvp.presenter.PicturePipettePresenter;
-import com.zqw.mobile.grainfull.R;
-import com.zqw.mobile.grainfull.mvp.ui.widget.colorpicker.ColorPickerPreference;
-import com.zqw.mobile.grainfull.mvp.ui.widget.colorpicker.ColorPickerView;
 import com.zqw.mobile.grainfull.mvp.ui.widget.colorpicker.ImageColorPickerView;
-import com.zqw.mobile.grainfull.mvp.ui.widget.colorpicker.builder.ColorPickerClickListener;
-import com.zqw.mobile.grainfull.mvp.ui.widget.colorpicker.builder.ColorPickerDialogBuilder;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
 
 /**
  * Description:图片吸管(图片中提取颜色)
@@ -40,16 +47,24 @@ import butterknife.OnClick;
  * @author 赤槿
  * module name is PicturePipetteActivity
  */
+@RuntimePermissions
 public class PicturePipetteActivity extends BaseActivity<PicturePipettePresenter> implements PicturePipetteContract.View {
     /*------------------------------------------------控件信息------------------------------------------------*/
     @BindView(R.id.activity_picture_pipette)
     LinearLayout contentLayout;                                                                     // 主布局
-
     @BindView(R.id.view_picturepipette_image)
-    ImageColorPickerView viewImage;
+    ImageColorPickerView viewImage;                                                                 // 提取颜色的容器
 
-    @BindView(R.id.txvi_picturepipette_show)
-    TextView txviShow;
+    @BindView(R.id.view_picturepipette_bg)
+    View viewBg;                                                                                    // 显示选中的颜色
+    @BindView(R.id.imvi_picturepipette_tips)
+    ImageView imviTips;                                                                             // 添加图片的提示
+    @BindView(R.id.btn_picturepipette_copy)
+    ImageView imviCopy;                                                                             // 复制按钮
+    @BindView(R.id.txvi_picturepipette_color)
+    TextView txviColor;                                                                             // 显示颜色值(十六进制)
+    @BindView(R.id.txvi_picturepipette_rgb)
+    TextView txviRgbColor;                                                                          // 显示颜色值(RGB)
     /*------------------------------------------------业务区域------------------------------------------------*/
 
     @Override
@@ -71,26 +86,90 @@ public class PicturePipetteActivity extends BaseActivity<PicturePipettePresenter
     public void initData(@Nullable Bundle savedInstanceState) {
         setTitle("图片吸管");
 
-        viewImage.setOnColorChangedListenner(new ImageColorPickerView.OnColorChangedListener() {
-            @Override
-            public void onColorChanged(int color) {
-                txviShow.setText(ColorsUtil.getHexString(color, false));
-                txviShow.setBackgroundColor(color);
+        viewImage.setOnColorChangedListenner(color -> {
+            // 显示背景色
+            viewBg.setBackgroundColor(color);
+            // 显示十六进制颜色
+            txviColor.setText(ColorsUtil.getHexString(color, false));
+            // 显示RGB颜色
+            txviRgbColor.setText("RGB " + Color.red(color) + "," + Color.green(color) + "," + Color.blue(color));
+
+            // 控制特殊控件的颜色
+            if (ColorUtils.isLightColor(color)) {
+                // 亮色
+                txviColor.setTextColor(getResources().getColor(R.color.colorpicker_txt_color));
+                txviRgbColor.setTextColor(getResources().getColor(R.color.colorpicker_txt_color));
+                imviCopy.setImageResource(R.mipmap.icon_copy);
+                imviTips.setImageResource(R.mipmap.icon_add_picture);
+            } else {
+                // 暗色
+                txviColor.setTextColor(Color.WHITE);
+                txviRgbColor.setTextColor(Color.WHITE);
+                imviCopy.setImageResource(R.mipmap.icon_copy_white);
+                imviTips.setImageResource(R.mipmap.icon_add_picture_white);
             }
+
         });
     }
 
-//    @OnClick({
-//            R.id.btn_picturepipette_click,                                                          // 点击
-//    })
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()){
-//            case R.id.btn_picturepipette_click:                                                     // 点击
-//
-//                break;
-//        }
-//    }
+    @OnClick({
+            R.id.btn_picturepipette_copy,                                                           // 复制
+            R.id.cola_picturepipette_select,                                                        // 选择图片
+    })
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_picturepipette_copy:                                                      // 复制
+                String mColor = txviColor.getText().toString();
+                if (!TextUtils.isEmpty(mColor)) {
+                    ClipboardUtils.copyText(mColor);
+                    showMessage("复制成功！");
+                }
+                break;
+            case R.id.cola_picturepipette_select:                                                   // 选择图片
+                PicturePipetteActivityPermissionsDispatcher.addAvatarWithPermissionCheck(this);
+                break;
+        }
+    }
+
+    /**
+     * 添加照片
+     */
+    @NeedsPermission({Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE})
+    public void addAvatar() {
+
+        ImagePicker.getInstance()
+                .setTitle("图片")//设置标题
+                .showCamera(true)//设置是否显示拍照按钮
+                .showImage(true)//设置是否展示图片
+                .showVideo(false)//设置是否展示视频
+                .filterGif(true)//设置是否过滤gif图片
+                .setMaxCount(1)//设置最大选择图片数目(默认为1，单选)
+                .setSingleType(true)//设置图片视频不能同时选择
+                .setImageLoader(new GlideLoader(this))//设置自定义图片加载器
+                .start(this, Constant.REQUEST_SELECT_IMAGES_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == Constant.REQUEST_SELECT_IMAGES_CODE) {
+                // 返回的参数
+                ArrayList<String> mImg = data.getStringArrayListExtra(ImagePicker.EXTRA_SELECT_IMAGES);
+
+                // 显示图片
+                viewImage.setImageBitmap(BitmapFactory.decodeFile(mImg.get(0)));
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PicturePipetteActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
 
     public Activity getActivity() {
         return this;
--- app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/widget/colorpicker/ImageColorPickerView.java
+++ app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/widget/colorpicker/ImageColorPickerView.java
@@ -8,11 +8,19 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.view.View;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.blankj.utilcode.util.ColorUtils;
+import com.blankj.utilcode.util.ConvertUtils;
 import com.zqw.mobile.grainfull.R;
+
+import timber.log.Timber;
 
 /**
  * @ProjectName: GrainFullAndroid
@@ -22,37 +30,37 @@ import com.zqw.mobile.grainfull.R;
  * @Author: WLY
  * @CreateDate: 2023/1/5 11:28
  */
-public class ImageColorPickerView extends View {
+public class ImageColorPickerView extends AppCompatImageView {
     // 句柄
-    private Context mContext;
+    private final Context mContext;
     // view高
     private int mHeight;
     // view宽
     private int mWidth;
     // 左侧宽度
     private int LEFT_WIDTH;
-    // 移动时的图标
+    // 暗色图标
     private Bitmap mLocationBitmap;
-    // 停止移动，选中后的图标
+    // 亮色图标
     private Bitmap mLocationPressBitmap;
     // 画笔
     private Paint mBitmapPaint;
-    // 左侧选择点
-    private PointF mLeftSelectPoint;
+    // 选中的位置
+    private PointF mSelectPoint;
     // 颜色变更监听事件
     private OnColorChangedListener mChangedListener;
-    // 是否移动中
-    private boolean isMove = false;
+    // 判断是否亮色
+    private boolean isLightColor = false;
     // 视图半径
     private float mBitmapRadius;
     // 渐变视图
     private Bitmap mGradualChangeBitmap;
     // 临时视图
     private Bitmap bitmapTemp;
-    // 图片的宽度
-    private int newWidgth;
-    // 图片的高度
-    private int newHeigh;
+    // 选中的颜色
+    private int mSelectColor;
+    // 初始后自动取一次
+    private boolean initAutoObtain = true;
 
     public ImageColorPickerView(Context context) {
         this(context, null);
@@ -73,15 +81,25 @@ public class ImageColorPickerView extends View {
      * 初始化资源与画笔
      */
     private void init() {
-        bitmapTemp = BitmapFactory.decodeResource(getResources(), R.drawable.text_image);
         mBitmapPaint = new Paint();
 
-        mLocationBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.front_back_switch);
-        mLocationPressBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.front_back_switch);
+        mLocationBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.icon_precise_target);
+        mLocationPressBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.icon_precise_target_press);
         mBitmapRadius = mLocationBitmap.getWidth() / 2;
-        mLeftSelectPoint = new PointF(0, 0);
-        newWidgth = BitmapFactory.decodeResource(getResources(), R.drawable.text_image).getWidth();
-        newHeigh = BitmapFactory.decodeResource(getResources(), R.drawable.text_image).getHeight();
+
+        BitmapDrawable drawable = (BitmapDrawable) getDrawable();
+        if (drawable == null) {
+            // 默认图
+            bitmapTemp = BitmapFactory.decodeResource(getResources(), R.drawable.text_image);
+        } else {
+            bitmapTemp = drawable.getBitmap();
+        }
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mSelectPoint = new PointF(w / 2, h / 2);
     }
 
     // important patient please!!!
@@ -89,15 +107,24 @@ public class ImageColorPickerView extends View {
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(getGradual(), null, new Rect(0, 0, LEFT_WIDTH, mHeight), mBitmapPaint);
 
-        // 是否移动
-        if (isMove) {
-            canvas.drawBitmap(mLocationBitmap, mLeftSelectPoint.x - mBitmapRadius, mLeftSelectPoint.y - mBitmapRadius, mBitmapPaint);
+        // 判断是否亮色
+        if (isLightColor) {
+            // 亮色用暗图标
+            canvas.drawBitmap(mLocationBitmap, mSelectPoint.x - mBitmapRadius, mSelectPoint.y - mBitmapRadius, mBitmapPaint);
         } else {
+            // 暗色用亮图标
             try {
-                canvas.drawBitmap(mLocationPressBitmap, mLeftSelectPoint.x - mBitmapRadius, mLeftSelectPoint.y - mBitmapRadius, mBitmapPaint);
+                canvas.drawBitmap(mLocationPressBitmap, mSelectPoint.x - mBitmapRadius, mSelectPoint.y - mBitmapRadius, mBitmapPaint);
             } catch (Exception e) {
                 // TODO: handle exception
             }
+        }
+
+        // 判断是否自动获取
+        if (initAutoObtain) {
+            initAutoObtain = false;
+            if (mChangedListener != null)
+                mChangedListener.onColorChanged(getLeftColor(mSelectPoint.x, mSelectPoint.y));
         }
     }
 
@@ -110,12 +137,12 @@ public class ImageColorPickerView extends View {
         if (widthMode == MeasureSpec.EXACTLY) {
             mWidth = width;
         } else {
-            mWidth = newHeigh;
+            mWidth = bitmapTemp.getWidth();
         }
         if (heightMode == MeasureSpec.EXACTLY) {
             mHeight = height;
         } else {
-            mHeight = newHeigh;
+            mHeight = bitmapTemp.getHeight();
         }
         LEFT_WIDTH = mWidth;
         setMeasuredDimension(mWidth, mHeight);
@@ -131,29 +158,38 @@ public class ImageColorPickerView extends View {
                 // 按下
             case MotionEvent.ACTION_MOVE:
                 // 移动
-                isMove = true;
+                // 获取当前选中的取色
+                mSelectColor = getLeftColor(x, y);
+                setControlIcon(mSelectColor);
+                if (mChangedListener != null)
+                    mChangedListener.onColorChanged(mSelectColor);
                 proofLeft(x, y);
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
-                // 取色
-                if (mChangedListener != null)
-                    mChangedListener.onColorChanged(getLeftColor(x, y));
-                isMove = false;
+                // 抬起
+                Timber.i("#### x=" + x + "  y=" + y);
                 invalidate();
         }
         return true;
     }
 
+    /**
+     * 根据当前颜色来控制应该显示哪个图标
+     */
+    private void setControlIcon(int mColor) {
+        isLightColor = ColorUtils.isLightColor(mColor);
+    }
+
     @Override
     protected void onDetachedFromWindow() {
-        if (mGradualChangeBitmap != null && mGradualChangeBitmap.isRecycled() == false) {
+        if (mGradualChangeBitmap != null && !mGradualChangeBitmap.isRecycled()) {
             mGradualChangeBitmap.recycle();
         }
-        if (mLocationBitmap != null && mLocationBitmap.isRecycled() == false) {
+        if (mLocationBitmap != null && !mLocationBitmap.isRecycled()) {
             mLocationBitmap.recycle();
         }
-        if (mLocationPressBitmap != null && mLocationPressBitmap.isRecycled() == false) {
+        if (mLocationPressBitmap != null && !mLocationPressBitmap.isRecycled()) {
             mLocationPressBitmap.recycle();
         }
         super.onDetachedFromWindow();
@@ -174,21 +210,24 @@ public class ImageColorPickerView extends View {
     // 校正xy
     private void proofLeft(float x, float y) {
         if (x < 0) {
-            mLeftSelectPoint.x = 0;
+            mSelectPoint.x = 0;
         } else if (x > (LEFT_WIDTH)) {
-            mLeftSelectPoint.x = LEFT_WIDTH;
+            mSelectPoint.x = LEFT_WIDTH;
         } else {
-            mLeftSelectPoint.x = x;
+            mSelectPoint.x = x;
         }
         if (y < 0) {
-            mLeftSelectPoint.y = 0;
+            mSelectPoint.y = 0;
         } else if (y > (mHeight - 0)) {
-            mLeftSelectPoint.y = mHeight - 0;
+            mSelectPoint.y = mHeight - 0;
         } else {
-            mLeftSelectPoint.y = y;
+            mSelectPoint.y = y;
         }
     }
 
+    /**
+     * 获取选中位置中的颜色
+     */
     private int getLeftColor(float x, float y) {
         Bitmap temp = getGradual();
         // 为了防止越界
@@ -203,12 +242,44 @@ public class ImageColorPickerView extends View {
             intY = temp.getHeight() - 1;
         }
 
-        System.out.println("leftColor" + temp.getPixel(intX, intY));
+        Timber.i("leftColor=%s", temp.getPixel(intX, intY));
         return temp.getPixel(intX, intY);
     }
 
     // ### 内部类 ###
     public interface OnColorChangedListener {
         void onColorChanged(int color);
+    }
+
+    @Override
+    public void setImageBitmap(Bitmap bm) {
+        super.setImageBitmap(bm);
+        this.bitmapTemp = bm;
+        mGradualChangeBitmap = null;
+        initAutoObtain = true;
+    }
+
+    @Override
+    public void setImageDrawable(@Nullable Drawable drawable) {
+        super.setImageDrawable(drawable);
+        this.bitmapTemp = ConvertUtils.drawable2Bitmap(drawable);
+        mGradualChangeBitmap = null;
+        initAutoObtain = true;
+    }
+
+    @Override
+    public void setImageResource(int resId) {
+        super.setImageResource(resId);
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        this.bitmapTemp = BitmapFactory.decodeResource(getResources(), resId, options);
+        mGradualChangeBitmap = null;
+        initAutoObtain = true;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmapTemp = bitmap;
+        mGradualChangeBitmap = null;
+        initAutoObtain = true;
     }
 }
