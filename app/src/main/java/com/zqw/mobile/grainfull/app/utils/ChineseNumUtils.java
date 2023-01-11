5f3470ffd32bd802e1ac41c9b3020969ad3eb6f3
wangliyang <wangliyang206@163.com>
2023-01-11 10:48:12 +0800

    内容变更： 1、完善 数字转中文 功能。

--- app/src/main/java/com/zqw/mobile/grainfull/app/utils/ChineseNumUtils.java
+++ app/src/main/java/com/zqw/mobile/grainfull/app/utils/ChineseNumUtils.java
@@ -1,135 +1,195 @@
 package com.zqw.mobile.grainfull.app.utils;
 
+import java.text.DecimalFormat;
+
 /**
  * @ProjectName: GrainFullAndroid
  * @Package: com.zqw.mobile.grainfull.app.utils
  * @ClassName: ChineseNumUtils
- * @Description:
+ * @Description: 数字转汉字
  * @Author: WLY
  * @CreateDate: 2023/1/10 17:52
  */
 public class ChineseNumUtils {
-    public static String[] chineseCode = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
-    private static String unit[][] = { { "", "万", "亿"}, { "", "拾", "佰", "仟"}};
-    private static String[] elseUnit = {"分", "角"};
+    // 汉字数字
+    private static String NUMBER[] = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九"};
+    // 大写数字
+    private final static String[] STR_NUMBER = {"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"};
+    // 整数单位
+    private final static String[] STR_UNIT = {"", "拾", "佰", "仟", "万", "拾", "佰", "仟", "亿", "拾", "佰", "仟"};
+    // 小数单位
+    private final static String[] STR_UNIT2 = {"厘", "分", "角"};
+
 
     /**
-     * 中文数字转阿拉伯数字
-     * (长度不能超过long最大值)
+     * 数字转汉字【新】
      *
-     * @param chNum 中文数字
-     * @return 阿拉伯数字
-     * @author Jarry Leo
+     * @param isCapital 是否大写
+     * @param num       数字
      */
-    public static long ch2Num(String chNum) {
-        // 对应下面单位后面多少个零
-        int[] numLen = {16, 8, 4, 3, 2, 1};
-        // 中文单位
-        String[] dw = {"兆", "亿", "万", "千", "百", "十"};
-        // 中文单位另一版
-        String[] dw1 = {"兆", "亿", "萬", "仟", "佰", "拾"};
-        // 中文数字
-        String[] sz = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"};
-        // 中文数字另一版
-        String[] sz1 = {"〇", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾"};
-
-        if (chNum == null) return 0;// 空对象返回0
-        for (int i = 0; i < sz.length; i++) { //统一文字版本
-            if (i < dw.length)
-                chNum = chNum.replaceAll(dw1[i], dw[i]);
-            chNum = chNum.replaceAll(sz1[i], sz[i]);
-        }
-
-        chNum = chNum.replaceAll("(百.)\\b", "$1十"); //正则替换为了匹配中文类似二百五这样的词
-        if (chNum.length() == 1) {
-            for (int i = 0; i < sz.length; i++) {
-                if (chNum.equals(sz[i])) return i;
-            }
-            return 0; //中文数字没有这个字
-        }
-        chNum = strReverse(chNum); //调转输入的字符串
-
-        for (int i = 0; i < dw.length; i++) {
-            if (chNum.contains(dw[i])) {
-                String part[] = chNum.split(dw[i], 2); //把字符串分割2部分
-                long num1 = ch2Num(strReverse(part[1]));
-                long num2 = ch2Num(strReverse(part[0]));
-                return (long) ((num1 == 0 ? 1 : num1) * Math.pow(10, numLen[i]) + num2);
-            }
-        }
-
-        char[] c = chNum.toCharArray();
-        long sum = 0;
-        for (int i = 0; i < c.length; i++) { //一个个解析数字
-            String tem = String.valueOf(c[i]); //根据索引转成对应数字
-            sum += ch2Num(tem) * Math.pow(10, i);//根据位置给定数字
-        }
-
-        return sum;
-    }
-
-    /**
-     * 字符串掉转
-     */
-    private static String strReverse(String str) {
-        return new StringBuilder(str).reverse().toString();
-    }
-
-    /**
-     * 人民币阿拉伯数字
-     */
-    public static String getRmb(double num) {
-        String s = "";
-        String numString = String.format("%.2f", num);
-        String[] strings = numString.split("\\.");
-        if (num == 0) {
-            s = "零圆整";
-        } else {
-            s += getRmbValue(false, strings[0]) + getRmbValue(true, strings[1]);
-        }
-        return s;
-    }
-
-    /**
-     * 返回格式化人民币结果
-     */
-    private static String getRmbValue(boolean isPoint, String snum) {
+    public static String numberToChinese(boolean isCapital, String num) {
         String result = "";
-        String[] numArray = snum.split("");
-        //判断是整数部分还是小数部分
-        if (isPoint) {
-            //小数部分
-            //判断是否是00
-            if (snum.equals("00")) {
-                // 00加圆整
-                result += "圆整";
+        String[] numArray = num.split("");
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i].indexOf(".") != -1) {
+                result += ("点");
             } else {
-                result += "圆";
-                //非00需要加角分单位
-                for (int i = 0; i < numArray.length; i++) {
-                    int value = Integer.valueOf(numArray[i]) % 10;
-                    result += (chineseCode[value] + elseUnit[numArray.length - 1 - i]);
+                int value = Integer.valueOf(numArray[i]) % 10;
+                if (isCapital) {
+                    // 大写
+                    result += (STR_NUMBER[value]);
+                } else {
+                    // 小写
+                    result += (NUMBER[value]);
                 }
-            }
-        } else {
-            //整数部分
-            int integerPart = (int) Math.floor(Double.valueOf(snum));
-            for (int i = 0; i < unit[0].length && integerPart > 0; i++) {
-                String p = "";
-                for (int j = 0; j < unit[1].length; j++) {
-                    //每次除以10确定当前大写汉字是什么
-                    p = chineseCode[integerPart % 10] + unit[1][j] + p;
-                    integerPart = integerPart / 10;
-                }
-                //使用正则去判断0
-                result = p.replaceAll("(零.)*零$", "") + unit[0][i] + result;
+
             }
         }
-        //把多余的零替换掉
-        result = result.replaceAll("(零.)+", "零");
-        if (result.substring(0, 1).equals("零")) {
+        return result;
+    }
+
+    /**
+     * 获取整数部分
+     *
+     * @param num 金额
+     * @return 金额整数部分
+     */
+    public static String getInteger(String num) {
+        // 判断是否包含小数点
+        if (num.indexOf(".") != -1) {
+            num = num.substring(0, num.indexOf("."));
+        }
+        // 反转字符串
+        num = new StringBuffer(num).reverse().toString();
+        // 创建一个StringBuffer对象
+        StringBuffer temp = new StringBuffer();
+        for (int i = 0; i < num.length(); i++) {// 加入单位
+            temp.append(STR_UNIT[i]);
+            temp.append(STR_NUMBER[num.charAt(i) - 48]);
+            /*
+             * num.charAt(i)-48获取数值或者使用Integer.pa……
+             * ASCLL表中0的位置是48，比如得到的字符3，对应ASCLL值 是51，减去48才是这里要的值
+             */
+        }
+        // 反转字符串
+        num = temp.reverse().toString();
+        // 替换字符串的字符
+        num = numReplace(num, "零拾", "零");
+        num = numReplace(num, "零佰", "零");
+        num = numReplace(num, "零仟", "零");
+        num = numReplace(num, "零万", "万");
+        num = numReplace(num, "零亿", "亿");
+        num = numReplace(num, "零零", "零");
+        num = numReplace(num, "亿万", "亿");
+        // 如果字符串以零结尾将其除去
+        if (num.lastIndexOf("零") == num.length() - 1) {
+            num = num.substring(0, num.length() - 1);
+        }
+        return num;
+    }
+
+    /**
+     * 获取小数部分
+     *
+     * @param num 金额
+     * @return 金额的小数部分
+     */
+    public static String getDecimal(String num) {
+        // 判断是否包含小数点
+        if (num.indexOf(".") == -1) {
+            return "";
+        }
+        num = num.substring(num.indexOf(".") + 1);
+        // 反转字符串
+        num = new StringBuffer(num).reverse().toString();
+        // 创建一个StringBuffer对象
+        StringBuffer temp = new StringBuffer();
+        // 加入单位
+        for (int i = 0; i < num.length(); i++) {
+            if (num.length() == 1) {
+                temp.append(STR_UNIT2[i + 2]);
+                temp.append(STR_NUMBER[num.charAt(i) - 48]);
+            } else if (num.length() == 2) {
+                temp.append(STR_UNIT2[i + 1]);
+                temp.append(STR_NUMBER[num.charAt(i) - 48]);
+            } else if (num.length() >= 3) {
+                temp.append(STR_UNIT2[i]);
+                temp.append(STR_NUMBER[num.charAt(i) - 48]);
+            }
+        }
+        // 反转字符串
+        num = temp.reverse().toString();
+        // 替换字符串的字符
+        num = numReplace(num, "零角", "零");
+        num = numReplace(num, "零分", "零");
+        num = numReplace(num, "零厘", "零");
+        num = numReplace(num, "零零", "零");
+
+        // 如果字符串以零结尾将其除去
+        if (num.lastIndexOf("零") == num.length() - 1) {
+            num = num.substring(0, num.length() - 1);
+        }
+        return num;
+    }
+
+    /**
+     * 替换字符串中内容
+     *
+     * @param num    字符串
+     * @param oldStr 被替换内容
+     * @param newStr 新内容
+     * @return 替换后的字符串
+     */
+    public static String numReplace(String num, String oldStr, String newStr) {
+        while (true) {
+            // 判断字符串中是否包含指定字符
+            if (num.indexOf(oldStr) == -1) {
+                break;
+            }
+            // 替换字符串
+            num = num.replaceAll(oldStr, newStr);
+        }
+        // 返回替换后的字符串
+        return num;
+    }
+
+    /**
+     * 金额转换
+     *
+     * @param d 金额
+     * @return 转换成大写的全额
+     */
+    public static String convert(double d) {
+        // 实例化DecimalFormat对象
+        DecimalFormat df = new DecimalFormat("#0.###");
+        // 格式化double数字
+        String strNum = df.format(d);
+        // 判断是否包含小数点
+        if (strNum.indexOf(".") != -1) {
+            String num = strNum.substring(0, strNum.indexOf("."));
+            // 整数部分大于12不能转换
+            if (num.length() > 12) {
+                System.out.println("数字太大，不能完成转换！");
+                return "";
+            }
+        }
+        String point = "";// 小数点
+        if (strNum.indexOf(".") != -1) {
+            point = "元";
+        } else {
+            if (strNum.length() > 12) {
+                System.out.println("数字太大，不能完成转换！");
+                return "";
+            }
+            point = "元整";
+        }
+        // 转换结果
+        String result = getInteger(strNum) + point + getDecimal(strNum);
+        if (result.startsWith("元")) { // 判断是字符串是否已"元"结尾
+            // 截取字符串
             result = result.substring(1, result.length());
         }
+        // 返回新的字符串
         return result;
     }
 }
--- app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/activity/DigitalToChineseActivity.java
+++ app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/activity/DigitalToChineseActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.blankj.utilcode.util.ClipboardUtils;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
@@ -26,6 +27,7 @@ import com.zqw.mobile.grainfull.mvp.contract.DigitalToChineseContract;
 import com.zqw.mobile.grainfull.mvp.presenter.DigitalToChinesePresenter;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Description:数字转中文
@@ -45,11 +47,11 @@ public class DigitalToChineseActivity extends BaseActivity<DigitalToChinesePrese
     @BindView(R.id.lila_digitaltochinese_result)
     LinearLayout lilaResult;                                                                        // 总布局，显示结果
     @BindView(R.id.txvi_digitaltochinese_lowercase)
-    TextView txviLowercase;                                                                         // 小写
+    TextView txviLowercase;                                                                         // 简体小写
     @BindView(R.id.txvi_digitaltochinese_capital)
-    TextView txviCapital;                                                                           // 大写
-    @BindView(R.id.txvi_digitaltochinese_amount)
-    TextView txviAmount;                                                                            // 金额
+    TextView txviCapital;                                                                           // 简体大写
+    @BindView(R.id.txvi_digitaltochinese_capitalamount)
+    TextView txviCapitalAmount;                                                                     // 大写金额
 
     /*------------------------------------------------业务区域------------------------------------------------*/
 
@@ -72,30 +74,89 @@ public class DigitalToChineseActivity extends BaseActivity<DigitalToChinesePrese
     public void initData(@Nullable Bundle savedInstanceState) {
         setTitle("数字转中文");
 
-        editInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // 增加输入监听
+        editInput.addTextChangedListener(mTextWatcher);
+    }
 
-            }
+    @OnClick({
+            R.id.lila_digitaltochinese_lowercase,                                                   // 复制 简体小写
+            R.id.lila_digitaltochinese_capital,                                                     // 复制 简体大写
+            R.id.lila_digitaltochinese_capitalamount,                                               // 复制 金额
+    })
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.lila_digitaltochinese_lowercase:                                              // 复制 简体小写
+                String mLowercase = txviLowercase.getText().toString();
+                if (!TextUtils.isEmpty(mLowercase)) {
+                    ClipboardUtils.copyText(mLowercase);
+                    showMessage("复制成功！");
+                }
+                break;
+            case R.id.lila_digitaltochinese_capital:                                                // 复制 简体大写
+                String mCapital = txviCapital.getText().toString();
+                if (!TextUtils.isEmpty(mCapital)) {
+                    ClipboardUtils.copyText(mCapital);
+                    showMessage("复制成功！");
+                }
+                break;
+            case R.id.lila_digitaltochinese_capitalamount:                                          // 复制 金额
+                String mAmount = txviCapitalAmount.getText().toString();
+                if (!TextUtils.isEmpty(mAmount)) {
+                    ClipboardUtils.copyText(mAmount);
+                    showMessage("复制成功！");
+                }
+                break;
+        }
+    }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (TextUtils.isEmpty(s)) {
-                    // 没有数字，则隐藏布局
-                    lilaResult.setVisibility(View.GONE);
+    /**
+     * 输入监听
+     */
+    private final TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (TextUtils.isEmpty(s)) {
+                // 没有数字，则隐藏布局
+                lilaResult.setVisibility(View.GONE);
+            } else {
+                // 判断第一位如果是“.”的话，则禁止输入
+                String str = s.subSequence(0, 1).toString();
+                if (str.equalsIgnoreCase(".")) {
+                    editInput.removeTextChangedListener(mTextWatcher);
+                    editInput.setText("");
+                    editInput.addTextChangedListener(mTextWatcher);
                 } else {
                     // 有数字，显示布局
                     lilaResult.setVisibility(View.VISIBLE);
-                    txviAmount.setText(ChineseNumUtils.getRmb(Double.parseDouble(s.toString())));
+
+                    // 简体小写
+                    txviLowercase.setText(ChineseNumUtils.numberToChinese(false, s.toString()));
+                    // 简体大写
+                    txviCapital.setText(ChineseNumUtils.numberToChinese(true, s.toString()));
+                    // 金额
+                    String capitalAmount = ChineseNumUtils.convert(Double.parseDouble(s.toString()));
+                    if (TextUtils.isEmpty(capitalAmount)) {
+                        showMessage("数字太大，不能完成转换！");
+                        txviCapitalAmount.setText("");
+                    } else {
+                        txviCapitalAmount.setText(capitalAmount);
+                    }
+
                 }
             }
+        }
 
-            @Override
-            public void afterTextChanged(Editable s) {
+        @Override
+        public void afterTextChanged(Editable s) {
 
-            }
-        });
-    }
+        }
+    };
 
     public Activity getActivity() {
         return this;
