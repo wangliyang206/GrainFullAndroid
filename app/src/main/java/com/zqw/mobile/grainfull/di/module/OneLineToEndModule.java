1d245840ba65997869e46d6f9e553cf8d36a0c80
wangliyang <wangliyang206@163.com>
2023-02-07 17:56:42 +0800

    内容变更： 1、【调整】完善一笔画完游戏(新增数据库操作)。

--- app/src/main/java/com/zqw/mobile/grainfull/di/module/OneLineToEndModule.java
+++ app/src/main/java/com/zqw/mobile/grainfull/di/module/OneLineToEndModule.java
@@ -1,12 +1,17 @@
 package com.zqw.mobile.grainfull.di.module;
 
-import com.jess.arms.di.scope.ActivityScope;
+import com.jess.arms.cj.ApiOperator;
+import com.jess.arms.cj.IRequestMapper;
+import com.jess.arms.di.scope.FragmentScope;
+import com.zqw.mobile.grainfull.app.global.AccountManager;
+import com.zqw.mobile.grainfull.app.global.RequestMapper;
+import com.zqw.mobile.grainfull.app.utils.DaoManager;
+import com.zqw.mobile.grainfull.mvp.contract.OneLineToEndContract;
+import com.zqw.mobile.grainfull.mvp.model.OneLineToEndModel;
 
 import dagger.Binds;
 import dagger.Module;
-
-import com.zqw.mobile.grainfull.mvp.contract.OneLineToEndContract;
-import com.zqw.mobile.grainfull.mvp.model.OneLineToEndModel;
+import dagger.Provides;
 
 /**
  * ================================================
@@ -21,4 +26,28 @@ public abstract class OneLineToEndModule {
 
     @Binds
     abstract OneLineToEndContract.Model bindOneLineToEndModel(OneLineToEndModel model);
+
+    @FragmentScope
+    @Provides
+    static AccountManager provideAccountManager(OneLineToEndContract.View view) {
+        return new AccountManager(view.getActivity());
+    }
+
+    @FragmentScope
+    @Provides
+    static IRequestMapper providerRequestMapper(OneLineToEndContract.View view, AccountManager mAccountManager) {
+        return new RequestMapper(view.getActivity(), mAccountManager);
+    }
+
+    @FragmentScope
+    @Provides
+    static ApiOperator providerOperator(IRequestMapper requestMapper) {
+        return new ApiOperator(requestMapper);
+    }
+
+    @FragmentScope
+    @Provides
+    static DaoManager providerDaoManager(OneLineToEndContract.View view) {
+        return new DaoManager(view.getActivity());
+    }
 }
