eefdc6de310e0f7584d6af80d4ebe32135bf7b3c
wangliyang <wangliyang206@163.com>
2023-02-17 18:17:20 +0800

    内容变更： 1、【新增】压缩图片功能增加按比例压缩。

--- app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/activity/PictureCompressionActivity.java
+++ app/src/main/java/com/zqw/mobile/grainfull/mvp/ui/activity/PictureCompressionActivity.java
@@ -5,6 +5,7 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,6 +13,7 @@ import android.text.format.Formatter;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,6 +22,7 @@ import androidx.annotation.Nullable;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.blankj.utilcode.util.FileUtils;
 import com.blankj.utilcode.util.ImageUtils;
+import com.blankj.utilcode.util.TimeUtils;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.http.imageloader.ImageLoader;
@@ -35,6 +38,7 @@ import com.zqw.mobile.grainfull.mvp.contract.PictureCompressionContract;
 import com.zqw.mobile.grainfull.mvp.presenter.PictureCompressionPresenter;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import javax.inject.Inject;
@@ -59,6 +63,11 @@ public class PictureCompressionActivity extends BaseActivity<PictureCompressionP
     /*------------------------------------------------控件信息------------------------------------------------*/
     @BindView(R.id.activity_picture_compression)
     LinearLayout contentLayout;                                                                     // 主布局
+
+    @BindView(R.id.radio_picturecompression_group)
+    RadioGroup radioGroup;                                                                          // 类型
+    @BindView(R.id.txvi_picturecompression_tips)
+    TextView txviTips;                                                                              // 提示
 
     @BindView(R.id.view_picturecompression_compression)
     LinearLayout btnCompression;                                                                    // 压缩按钮
@@ -122,6 +131,14 @@ public class PictureCompressionActivity extends BaseActivity<PictureCompressionP
         // 初始化Loading对话框
         mDialog = new MaterialDialog.Builder(this).content(R.string.common_execute).progress(true, 0).build();
 
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            if (group.getCheckedRadioButtonId() == R.id.radio_picturecompression_size) {
+                txviTips.setText("说明：低于500kB的照片不支持压缩。");
+            } else {
+                txviTips.setText("说明：将图片尺寸压缩20%。");
+            }
+        });
+
         // 友盟统计 - 自定义事件
         MobclickAgent.onEvent(getApplicationContext(), "picture_compression");
     }
@@ -137,17 +154,59 @@ public class PictureCompressionActivity extends BaseActivity<PictureCompressionP
                 PictureCompressionActivityPermissionsDispatcher.addAvatarWithPermissionCheck(this);
                 break;
             case R.id.view_picturecompression_compression:                                          // 压缩图片
-                if (CommonUtils.isNotEmpty(mImage)) {
-                    compressImage(mImage);
-                } else {
-                    showMessage("请先选择图片！");
-                }
+                onCompression();
                 break;
         }
     }
 
     /**
-     * 压缩图片
+     * 图片压缩
+     */
+    private void onCompression() {
+        if (CommonUtils.isNotEmpty(mImage)) {
+            if (radioGroup.getCheckedRadioButtonId() == R.id.radio_picturecompression_size) {
+                // 按大小
+                compressImage(mImage);
+            } else {
+                // 按比例
+                if (mDialog != null)
+                    mDialog.show();
+                new Thread(() -> {
+                    // 缩放
+                    Bitmap mBitmap = ImageUtils.compressByScale(BitmapFactory.decodeFile(mImage.get(0)), 0.8f, 0.8f);
+                    // 生成文件路径及名称
+                    String path = Constant.IMAGE_PATH + TimeUtils.getNowString(new SimpleDateFormat("yyyyMMdd_HHmmss")) + ".png";
+                    // 保存图片
+                    ImageUtils.save(mBitmap, path, Bitmap.CompressFormat.PNG);
+
+                    runOnUiThread(() -> {
+                        if (mDialog != null)
+                            mDialog.cancel();
+                        // 压缩成功后调用，返回压缩后的图片文件
+                        // 显示图片路径
+                        txviPath.setText("压缩后保存路径：" + path);
+
+                        // 获取图片尺寸
+                        int[] imageSize = ImageUtils.getSize(path);
+                        // 获取图片大小
+//                        String fileSize = FileUtils.getSize(path);
+                        long fileLength = FileUtils.getLength(path);
+                        String fileSize = Formatter.formatFileSize(getApplicationContext(), fileLength);
+                        txviAfterSize.setText(imageSize[0] + "*" + imageSize[1]);
+                        txviAfterHowBig.setText(fileSize);
+                    });
+                }).start();
+
+            }
+        } else {
+            showMessage("请先选择图片！");
+        }
+
+    }
+
+
+    /**
+     * 按大小压缩图片
      */
     private void compressImage(ArrayList<String> mImg) {
         Luban.with(this)
