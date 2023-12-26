bbe46ae627d5741586f1823dda6fe1c10f2af3f8
wangliyang <wangliyang206@163.com>
2023-12-26 12:35:54 +0800

    内容变更： 1、完善FastGPT。

--- app/src/main/java/com/zqw/mobile/grainfull/mvp/presenter/FastGptModelsPresenter.java
+++ app/src/main/java/com/zqw/mobile/grainfull/mvp/presenter/FastGptModelsPresenter.java
@@ -58,7 +58,7 @@ public class FastGptModelsPresenter extends BasePresenter<FastGptModelsContract.
     private ChatCompletionChunk chatCompletionChunk;
     private Gson gson = new Gson();
     // 默认提示语
-    private final String mDefaultTips = "您好，我是豆芽AI小助手，请问有什么可以帮助您的吗？";
+    private final String mDefaultTips = "您好，我是你的全能助手豆芽，目前我拥有【查询天气】、【查看微博热搜】、【智能聊天】功能。";
 
     @Override
     public void onDestroy() {
